c5e903a7e2c47ff4a838ad9745e842e3a38b7245
Akos Balasko <akosbalasko@akos-mbp.t.hu>
2020-05-30 23:27:45 +0200

    span replacement standardization and style-fix

--- src/utils/turndown-rules/span.ts
+++ src/utils/turndown-rules/span.ts
@@ -4,7 +4,8 @@ import { Rule } from 'turndown';
 import { filterByNodeName } from './filter-by-nodename';
 import { getAttributeProxy } from './get-attribute-proxy';
 const EVERNOTE_HIGHLIGHT = '-evernote-highlight:true;';
-const FONT_BOLD = 'font-weight:700';
+const FONT_WEIGHT_BOLD_NUMBER = 'font-weight:700';
+const FONT_WEIGHT_BOLD_KEYWORD = 'font-weight:bold';
 
 export const spanRule: Rule = {
     filter: filterByNodeName('SPAN'),
@@ -12,14 +13,14 @@ export const spanRule: Rule = {
         const nodeProxy = getAttributeProxy(node);
         if (nodeProxy.style) {
             const nodeValue: string = nodeProxy.style.value;
-            if(nodeValue.includes(EVERNOTE_HIGHLIGHT)) {
-                content = `<span style="background-color:yellow">${content}</span>`
-            }
-            if(nodeValue.includes(FONT_BOLD)){
-                content = ` **${content}**`
-            }
-            return content;
+
+            return nodeValue.includes(EVERNOTE_HIGHLIGHT) ?
+                `\`${content}\`` :
+                ((nodeValue.includes(FONT_WEIGHT_BOLD_NUMBER) || nodeValue.includes(FONT_WEIGHT_BOLD_KEYWORD)) ?
+                 ` **${content}**` :
+                content);
         }
+
+        return content;
     },
 };
-
