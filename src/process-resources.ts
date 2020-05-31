51f4b9221f68ac0e7ccfd796ae77ad9681d181a3
Akos Balasko <akosbalasko@akos-mbp.t.hu>
2020-05-31 22:04:54 +0200

    fix to break if no resource-attributes properties

--- src/process-resources.ts
+++ src/process-resources.ts
@@ -58,22 +58,23 @@ const processResource = (workDir: string, resource: any): any => {
     const resourceHash: any = {};
     const data = resource['data'];
 
-    const timeStamp = resource['resource-attributes']['timestamp'];
-    const fileName = resource['resource-attributes']['file-name'];
-    if (fileName) {
+    if (resource['resource-attributes'] && resource['resource-attributes']['file-name']) {
+      const timeStamp = resource['resource-attributes']['timestamp'];
+      const fileName = resource['resource-attributes']['file-name'];
       const absFilePath = `${workDir}/${fileName}`;
       // tslint:disable-next-line: curly
       if (resource['recognition'] && resource['recognition']['__cdata'] && fileName) {
-      const hashIndex = resource['recognition']['__cdata'].match(/[a-f0-9]{32}/);
-      resourceHash[hashIndex as any] = fileName;
-
+        const hashIndex = resource['recognition']['__cdata'].match(/[a-f0-9]{32}/);
+        resourceHash[hashIndex as any] = fileName;
       } else {
         resourceHash['any'] = fileName;
       }
-      const accessTime = moment(timeStamp);
+
+      const accessTime = timeStamp ? moment(timeStamp) : moment();
       fs.writeFileSync(absFilePath, data, 'base64');
       const atime = accessTime.valueOf() / 1000;
       fs.utimesSync(absFilePath, atime, atime);
+
     }
 
     return resourceHash;
