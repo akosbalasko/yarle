fe57300278930d330185e23309933e763ea4038e
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-10-19 18:18:56 -0300

    code block rule

--- src/utils/turndown-rules/code-block-rule.ts
+++ src/utils/turndown-rules/code-block-rule.ts
@@ -1,16 +1,18 @@
-import { Rule } from 'turndown';
-
 import { filterByNodeName } from './filter-by-nodename';
 import { getAttributeProxy } from './get-attribute-proxy';
 
+const mdCodeBlockFence = '\n```\n';
+const codeBlockFlag = '-en-codeblock:true';
+
 export const codeBlockRule = {
     filter: filterByNodeName('DIV'),
-    replacement: (content: any, node: any) => {
+    replacement: (content: string, node: any) => {
         const nodeProxy = getAttributeProxy(node);
-        if (nodeProxy.style && nodeProxy.style.value.indexOf('-en-codeblock:true')) {
-            return "\n```\n" + content + "\n```\n";
+
+        if (nodeProxy.style && nodeProxy.style.value.indexOf(codeBlockFlag) >= 0) {
+            return `${mdCodeBlockFence}${content}${mdCodeBlockFence}`;
         }
 
-        return content;
+        return `\n${content}`;
     },
 };
