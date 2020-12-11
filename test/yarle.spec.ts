d89e544b449c1e7a32ebfd97b412148432adbf09
Akos Balasko <akos0215@gmail.com>
2020-12-11 23:33:01 +0100

    SkipLocation

--- test/yarle.spec.ts
+++ test/yarle.spec.ts
@@ -468,13 +468,13 @@ describe('dropTheRope ', async () => {
     await yarle.dropTheRope(options);
     assert.equal(
       fs.existsSync(
-        `${__dirname}/../out/notes/test-skipLocation/skiplocation.md`,
+        `${__dirname}/../out/notes/test-skipLocation/SkipLocation.md`,
       ),
       true,
     );
     assert.equal(
       fs.readFileSync(
-        `${__dirname}/../out/notes/test-skipLocation/skiplocation.md`,
+        `${__dirname}/../out/notes/test-skipLocation/SkipLocation.md`,
         'utf8',
       ),
       fs.readFileSync(`${__dirname}/data/test-skipLocation.md`, 'utf8'),
