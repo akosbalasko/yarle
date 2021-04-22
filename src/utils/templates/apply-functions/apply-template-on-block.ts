b594c3401d78f3819822e0b50c85692213c55619
Akos Balasko <akos0215@gmail.com>
2021-04-22 23:53:32 +0200

    fix in tags

--- src/utils/templates/apply-functions/apply-template-on-block.ts
+++ src/utils/templates/apply-functions/apply-template-on-block.ts
@@ -16,7 +16,7 @@ export const applyTemplateOnBlock = ({
       .replace(new RegExp(`${valuePlaceholder}`,'g'), value);
       
   }
-const reg = `${startBlockPlaceholder}([\\d\\D])(?:.|\n)*?(?=${endBlockPlaceholder})${endBlockPlaceholder}`;
+const reg = `${startBlockPlaceholder}([\\d\\D])(?:.|\n\r)*?(?=${endBlockPlaceholder})${endBlockPlaceholder}`;
   return template.replace(
     new RegExp(reg,
       'g',
