82b356b8731acfb7e35ec59df8a28c75d74b90a0
Guoboism <guoboism@hotmail.com>
2021-08-22 10:46:45 +0800

    Chain command, remove empty space, md link, etc

--- src/utils/turndown-rules/span-rule.ts
+++ src/utils/turndown-rules/span-rule.ts
@@ -14,16 +14,16 @@ export const spanRule = {
         const HIGHLIGHT_SEPARATOR = yarleOptions.outputFormat === OutputFormat.ObsidianMD ? '==' : '`' ;
         const nodeProxy = getAttributeProxy(node);
         if (nodeProxy.style) {
-            const nodeValue: string = nodeProxy.style.value; 
+            const nodeValue: string = nodeProxy.style.value;
 
             if(yarleOptions.logseqMode){
                 //this aims to care for bold text generated as <span style="font-weight: bold;">Bold</span>
                 if(content != "<YARLE_NEWLINE_PLACEHOLDER>"){
                     const has_bold =  nodeValue.includes(BOLD);
                     const has_italic =  nodeValue.includes(ITALIC);
-                    if(has_bold && !has_italic) return "**"+content+"**";
-                    if(!has_bold && has_italic) return "_"+content+"_";
-                    if(has_bold && has_italic) return "_**"+content+"**_";
+                    if(has_bold && !has_italic) return `**${content}**`;
+                    if(!has_bold && has_italic) return `_${content}_`;
+                    if(has_bold && has_italic) return `_**${content}**_`;
                 }
             }
 
