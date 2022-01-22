3505c8143cde59a5c0ed322de2796c9153d33075
João Sousa Andrade <sousa.andrade.joao@gmail.com>
2022-01-22 21:26:31 +0000

    Fix: en-todo wrapping and content lost on nested lists (#340)

--- src/convert-html-to-md.ts
+++ src/convert-html-to-md.ts
@@ -5,32 +5,70 @@ import { NoteData } from './models/NoteData';
 import { YarleOptions } from './YarleOptions';
 import { OutputFormat } from './output-format';
 
+const unwrapElement = (node: Element) => {
+    node.replaceWith(...Array.from(node.children));
+};
+
+const swapParent = (wrapper: Element) => {
+    const inner = wrapper.parentElement;
+    inner.replaceWith(...Array.from(inner.childNodes));
+    inner.append(...Array.from(wrapper.childNodes));
+    wrapper.appendChild(inner);
+};
+
+const fixTasks = (node: HTMLElement) => {
+    // fix bold or italic breaking a task's syntax
+    // i.e. '*[] foo*' instead of '[] *foo*'
+    const spanTasks = Array.from(node.querySelectorAll('span>en-todo'));
+    spanTasks.forEach(swapParent);
+    // fix an anchor breaking a task's syntax
+    // i.e. '[] [foo](bar)' instead of '[[] foo](bar)'
+    const anchorTasks = Array.from(node.querySelectorAll('a>en-todo'));
+    anchorTasks.forEach(swapParent);
+
+    return node;
+};
+
 const fixSublists = (node: HTMLElement) => {
     const ulElements: Array<HTMLElement> = Array.from(node.getElementsByTagName('ul'));
     const olElements: Array<HTMLElement> = Array.from(node.getElementsByTagName('ol'));
     const listElements = ulElements.concat(olElements);
+
     listElements.forEach(listNode => {
-      if (listNode.parentElement.tagName === 'LI') {
-        listNode.parentElement.replaceWith(listNode);
-      }
-      if (
-        listNode.previousElementSibling &&
-        listNode.previousElementSibling.tagName === 'LI'
-      ) {
-        // The below moves, not copies. https://stackoverflow.com/questions/7555442/move-an-element-to-another-parent-after-changing-its-id
-        listNode.previousElementSibling.appendChild(listNode);
-      }
+        if (listNode.parentElement.tagName === 'LI') {
+            listNode.parentElement.replaceWith(listNode);
+        }
+        if (
+            listNode.previousElementSibling &&
+            listNode.previousElementSibling.tagName === 'LI'
+        ) {
+            // The below moves, not copies. https://stackoverflow.com/questions/7555442/move-an-element-to-another-parent-after-changing-its-id
+            listNode.previousElementSibling.appendChild(listNode);
+        }
     });
+
+    for (const n of listElements) {
+        const parentElement = n.parentElement;
+        if (parentElement?.tagName === 'DIV' &&
+            parentElement?.parentElement?.tagName === 'UL') {
+            unwrapElement(parentElement);
+        }
+        // remove nested lists whose only child is another list, i.e. <ul><ul>...</ul></ul>
+        if ((parentElement?.tagName === 'UL' || parentElement?.tagName === 'OL')
+            && parentElement?.childNodes.length === 1) {
+            unwrapElement(parentElement);
+        }
+    }
 
     // The contents of every EN list item are wrapped by a div element. `<li><div>foo</div></li>`
     // We need to remove this `<div>`, since it's a block element and will lead to unwanted whitespace otherwise
     const liElements: Array<HTMLElement> = Array.from(node.getElementsByTagName('li'));
     for (const liNode of liElements) {
-      const listNodeDiv = liNode.firstElementChild;
-      if (listNodeDiv && listNodeDiv.tagName === 'DIV') {
-        const childElementsArr = Array.from(listNodeDiv.childNodes);
-        listNodeDiv.replaceWith(...childElementsArr);
-      }
+        const listNodeDiv = liNode.firstElementChild;
+        if (listNodeDiv && listNodeDiv.tagName === 'DIV') {
+            const childElementsArr = Array.from(listNodeDiv.childNodes);
+            listNodeDiv.replaceWith(...childElementsArr);
+        }
     }
 
     return node;
@@ -44,7 +82,8 @@ export const convertHtml2Md = (yarleOptions: YarleOptions, { htmlContent }: Note
     const contentNode = new JSDOM(content).window.document
       .getElementsByTagName('en-note').item(0) as any as HTMLElement;
 
-    let contentInMd = getTurndownService(yarleOptions).turndown(fixSublists(contentNode));
+    let contentInMd = getTurndownService(yarleOptions)
+        .turndown(fixTasks(fixSublists(contentNode)));
 
     const newLinePlaceholder = new RegExp('<YARLE_NEWLINE_PLACEHOLDER>', 'g');
     contentInMd = contentInMd.replace(newLinePlaceholder, '');
