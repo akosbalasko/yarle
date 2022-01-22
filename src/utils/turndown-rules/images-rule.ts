f57fbf18746b3c57f27501fe71c46e4eb1e4c028
João Sousa Andrade <sousa.andrade.joao@gmail.com>
2022-01-22 21:25:47 +0000

    Fix: Obsidian image sizes (#350)

--- src/utils/turndown-rules/images-rule.ts
+++ src/utils/turndown-rules/images-rule.ts
@@ -28,9 +28,8 @@ export const imagesRule = {
 
       return `![](${realValue}${sizeString})`;
     } else if (yarleOptions.keepImageSize === OutputFormat.ObsidianMD) {
+      sizeString = (widthParam || heightParam) ? `|${widthParam || 0}x${heightParam || 0}` : '';
       if (realValue.startsWith('./')) {
-        sizeString = (widthParam || heightParam) ? `|${widthParam}x${heightParam}` : '';
-
         return `![[${realValue}${sizeString}]]`;
       } else {
         return `![${sizeString}](${realValue})`;
