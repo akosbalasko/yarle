9930d6ef77e6ca011d028cf2aa4766da11f21250
Akos Balasko <akos0215@gmail.com>
2024-01-06 09:43:37 +0100

    fix: embedded file generalization fixed - alt used (#571)

--- src/process-resources.ts
+++ src/process-resources.ts
@@ -110,10 +110,17 @@ export const extractDataUrlResources = (
 
   const {absoluteResourceWorkDir, relativeResourceWorkDir} = getResourceWorkDirs(note);
   fsExtra.mkdirsSync(absoluteResourceWorkDir);
+ 
+  // change links hrefs
+  content = content.replace(/ (href)="data:([^;,]*)(;base64)?,([^"]*)"(.*?)<\/a>/g, (match,argument, mediatype, encoding, data, postfix) => {
+    const fileName = createResourceFromData(argument, match, mediatype, encoding === ';base64', data, absoluteResourceWorkDir, note);
+    const argValue = `${relativeResourceWorkDir}${yarleOptions.pathSeparator}${fileName}`;
 
+    return `yarle-file-resource="true" ${argument}="${argValue}"${postfix}`;
+  });
   // src|href="data:image/svg+xml;base64,..." --> src="resourceDir/fileName"
-  return content.replace(/([^= ]*)="data:([^;,]*)(;base64)?,([^"]*)"/g, (match,argument, mediatype, encoding, data) => {
-    const fileName = createResourceFromData(mediatype, encoding === ';base64', data, absoluteResourceWorkDir, note);
+  return content.replace(/(src)="data:([^;,]*)(;base64)?,([^"]*)"/g, (match,argument, mediatype, encoding, data) => {
+    const fileName = createResourceFromData(argument, match, mediatype, encoding === ';base64', data, absoluteResourceWorkDir, note);
     const argValue = `${relativeResourceWorkDir}${yarleOptions.pathSeparator}${fileName}`;
 
     return `yarle-file-resource="true" ${argument}="${argValue}"`;
@@ -122,6 +129,8 @@ export const extractDataUrlResources = (
 
 // returns filename of new resource
 const createResourceFromData = (
+  argument: string,
+  match: string,
   mediatype: string,
   base64: boolean,
   data: string,
@@ -131,7 +140,14 @@ const createResourceFromData = (
   const baseName = 'embedded'; // data doesn't seem to include useful base filename
   const extension = extensionForMimeType(mediatype) || '.dat';
   const index = utils.getFileIndex(absoluteResourceWorkDir, baseName);
-  const fileName = index < 1 ? `${baseName}.${extension}` : `${baseName}.${index}.${extension}`;
+  let fileName = index < 1 ? `${baseName}.${extension}` : `${baseName}.${index}.${extension}`;
+
+
+  if (argument === 'href'){
+    const baseNameCandidate = match.split("alt=\"")[1]?.split("\"")[0]
+    if (baseNameCandidate)
+    fileName = baseNameCandidate
+  }
   const absFilePath = `${absoluteResourceWorkDir}${path.sep}${fileName}`;
 
   if (!base64 && !data.startsWith('&lt;svg')) { // TODO: handle <svg, tags, they may be checkboxes
