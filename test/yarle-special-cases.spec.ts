b0a8b868f1ce490cf08b992a526ca949a8cf88d3
Akos Balasko <akos0215@gmail.com>
2024-01-13 16:24:07 +0100

    fix: encryption fixed (#582)

--- test/yarle-special-cases.spec.ts
+++ test/yarle-special-cases.spec.ts
@@ -165,6 +165,33 @@ dateFormat: undefined,
     );
   });
 
+  it('Encrypted lines in notes - no such password', async () => {
+    const options: YarleOptions = {
+dateFormat: undefined,
+      enexSources: [ `.${path.sep}test${path.sep}data${path.sep}test-encryption.enex` ],
+      outputDir: 'out',
+      outputFormat: OutputFormat.ObsidianMD,
+      convertPlainHtmlNewlines: true,
+      isMetadataNeeded: true,
+      encryptionPasswords: ["wrong_password"]
+    };
+    await yarle.dropTheRope(options);
+    // tslint:disable-next-line:no-console
+    assert.equal(
+      fs.existsSync(
+        `${__dirname}/../out/notes/test-encryption/Encryption.md`,
+      ),
+      true,
+    );
+
+    assert.equal(
+      eol.auto(fs.readFileSync(
+        `${__dirname}/../out/notes/test-encryption/Encryption.md`,
+        'utf8',
+      )),
+      fs.readFileSync(`${__dirname}/data/test-encryption-nopassword.md`, 'utf8'),
+    );
+  });
   it('Enex file with note containing a picture', async () => {
     const options: YarleOptions = {
 dateFormat: undefined,
